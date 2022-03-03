1f45cdc2016258545ce2d90b17f2adda697e5a99
유민호 <wingedcompany@gmail.com>
2022-03-03 22:47:47 +0900

    크롤링 2

--- 03_cheerio/index.js
+++ 03_cheerio/index.js
@@ -4,20 +4,25 @@ import xlsx from 'xlsx';
 
 const workbook = xlsx.readFile('02_excel/xlsx/data.xlsx');
 const ws = workbook.Sheets.영화목록;
+
 const records = xlsx.utils.sheet_to_json(ws);
 
 const crawler = (records) => {
     return Promise.all(records.map(async (r) => {
-        const response = await axios.get(r.링크);
-        if(response.status === 200) {
-            const html = response.data;
-            const $ = cheerio.load(html);
-            r.point = $("#pointNetizenPersentBasic").text();
+        try {
+            const response = await axios.get(r.링크);
+            if (response.status === 200) {
+                const html = response.data;
+                const $ = cheerio.load(html);
+                r.point = $("#pointNetizenPersentBasic").text();
+            }
+            return r;
+        } catch (e){
+            const {status, statusText} = e.response;
+            console.error('error', status, statusText);
         }
-        return r;
     }))
 }
 
 const result = await crawler(records);
-
 console.log(result);
