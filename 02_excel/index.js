ca5736f67c1ccb7fab684b62209105d53b4a125e
유민호 <wingedcompany@gmail.com>
2022-03-21 00:52:31 +0900

    1. 웹 크롤링과 데이터 파싱

--- 02_excel/index.js
+++ 02_excel/index.js
@@ -1,7 +1,9 @@
 import xlsx from 'xlsx';
 
 const workbook = xlsx.readFile('02_excel/xlsx/data.xlsx');
-const ws = workbook.Sheets.영화목록;
-const records = xlsx.utils.sheet_to_json(ws);
 
-console.log(records);
+workbook.SheetNames.forEach(v => {
+    const ws = workbook.Sheets[v];
+    const records = xlsx.utils.sheet_to_json(ws);
+    console.log(records);
+})
--- 03_cheerio/index.js
+++ 03_cheerio/index.js
@@ -1,28 +1,28 @@
 import axios from "axios";
 import cheerio from 'cheerio';
 import xlsx from 'xlsx';
+import {add_to_sheet} from "./add_to_sheet.js";
 
 const workbook = xlsx.readFile('02_excel/xlsx/data.xlsx');
-const ws = workbook.Sheets.영화목록;
 
-const records = xlsx.utils.sheet_to_json(ws);
-
-const crawler = (records) => {
-    return Promise.all(records.map(async (r) => {
-        try {
-            const response = await axios.get(r.링크);
-            if (response.status === 200) {
-                const html = response.data;
-                const $ = cheerio.load(html);
-                r.point = $("#pointNetizenPersentBasic").text();
-            }
-            return r;
-        } catch (e){
-            const {status, statusText} = e.response;
-            console.error('error', status, statusText);
+const crawler = async (ws, records) => {
+    add_to_sheet(ws, 'C1', 'S', '평점');
+    for (const [i, r] of records.entries()) {
+        const response = await axios.get(r.링크);
+        if (response.status === 200) {
+            const html = response.data;
+            const $ = cheerio.load(html);
+            const text = $("#pointNetizenPersentBasic").text();
+            const newCell = `C${i+2}`
+            add_to_sheet(ws, newCell, 'S', parseFloat(text.trim()));
         }
-    }))
+    }
+    xlsx.writeFile(workbook, '02_excel/xlsx/result.xlsx');
 }
 
-const result = await crawler(records);
-console.log(result);
+workbook.SheetNames.forEach(async v => {
+    const ws = workbook.Sheets[v];
+    const records = xlsx.utils.sheet_to_json(ws);
+    const result = await crawler(ws, records);
+    console.log(result);
+})
