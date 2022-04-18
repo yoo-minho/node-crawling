8f516e77f570496f311402847d47a1bb4701557b
유민호 <wingedcompany@gmail.com>
2022-04-19 00:02:45 +0900

    다시 시작한 크롤링 1일차

--- 04_puppeteer/index.js
+++ 04_puppeteer/index.js
@@ -2,30 +2,44 @@ import csv from 'csv-parser';
 import fs from 'fs';
 import puppeteer from 'puppeteer'
 
-const crawler = async () => {
-    const browser = await puppeteer.launch({headless:false})
-    const page = await browser.newPage();
-    const page2 = await browser.newPage();
-    await Promise.all([
-        page.goto('http://flow.team'),
-        page2.goto('http://naver.com'),
-    ])
-    await page.waitForTimeout(3000);
-    await page2.waitForTimeout(1000);
-    await page.close();
-    await page2.close();
-    await browser.close();
+const crawler = async (records) => {
+
+    const isProduction = process.env.NODE_ENV === 'production';
+    const browser = await puppeteer.launch({headless: true})
+
+    try {
+        await Promise.all(records.map(async (v) => {
+            try {
+                const page = await browser.newPage();
+                await page.goto(v.Url);
+                const result = await page.evaluate(() => {
+                    const scoreEl = document.querySelector('.score.score_left .star_score')
+                    if (scoreEl) {
+                        return {
+                            score: scoreEl.textContent.trim()
+                        };
+                    }
+                })
+                console.log(result);
+                await page.close();
+            } catch (e) {
+                console.error(e);
+            }
+        }))
+        await browser.close();
+    } catch (e) {
+        console.error(e);
+    }
 }
 
-crawler();
 
 const records = [];
 fs.createReadStream('01_csv/csv/data.csv')
     .pipe(csv(['Name', 'Url']))
     .on('data', (data) => {
-        records.push(data)
+        records.push(data);
     })
     .on('end', () => {
-        console.log('records', records);
+        crawler(records);
     });
 
