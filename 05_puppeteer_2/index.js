60bdb50260747c2ee9232e6bb0c7b4eeb2a26efd
유민호 <wingedcompany@gmail.com>
2022-04-23 14:04:54 +0900

    다시 시작한 크롤링 4일차

--- 05_puppeteer_2/index.js
+++ 05_puppeteer_2/index.js
@@ -2,29 +2,58 @@ import puppeteer from 'puppeteer';
 
 const crawler = async () => {
     try {
-        const browser = await puppeteer.launch({headless: false})
+        const browser = await puppeteer.launch({headless: true})
         const page = await browser.newPage();
         await page.goto('https://unsplash.com');
-        const result = await page.evaluate(() => {
-            const imgEls = document.querySelectorAll('figure[itemprop=image]');
-            if (imgEls.length > 0) {
-                return [...imgEls].map(v => {
-                    const url = v.querySelector('img').src;
-                    v.parentElement.removeChild(v);
-                    return url;
-                });
+        let results = [];
+
+        let src;
+        let cycle = 0;
+        while (cycle < 10) {
+            console.log("cycle", cycle, results.length);
+            try {
+                src = await getSrc(page);
+                results = [...results, ...src];
+            } catch (e) {
+                cycle = 10;
             }
-            window.scrollBy(0, 1000);
-        })
-        console.log(result);
-        await page.waitForSelector('figure[itemprop=image]');
-        console.log('태그 로딩 완료');
-        //await page.close();
-        //await browser.close();
+            if (results.length > 100) {
+                cycle = 10;
+            }
+            cycle++;
+        }
+
+        console.log(results.length, results);
+        await page.close();
+        await browser.close();
     } catch (e) {
         console.error(e);
     }
 
+    async function getSrc(page) {
+        try {
+            const src = await page.evaluate(() => {
+                window.scrollBy(0, 0);
+                const imgEls = document.querySelectorAll('figure[itemprop=image]');
+                let data = [];
+                if (imgEls.length > 0) {
+                    data = [...imgEls].map(v => {
+                        const url = v.querySelector('img').src;
+                        v.parentElement.removeChild(v);
+                        return url;
+                    });
+                }
+                setTimeout(() => {
+                    window.scrollBy(0, 300);
+                }, 1000)
+                return data;
+            })
+            await page.waitForSelector('figure[itemprop=image]', {timeout: 3000});
+            return src;
+        } catch (e) {
+            return [];
+        }
+    }
 }
 
 crawler();
